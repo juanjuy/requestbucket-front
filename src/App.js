9446a27cbd0e3d264e20a367efb5964d69591018
juanjuy <alex.juanjuy@gmail.com>
2022-06-03 11:43:48 -0700

    logo credit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CreateButton from './components/CreateButton';
 import CreatedPage from './components/CreatedPage';
 import History from './components/History';
+import bucket from './images/bucket.png'
 
 function App() {
   return (
     <Router>
+      <img src={bucket} alt="cartoon bucket"></img>
       <Link to="/"><h1>RequestBucket</h1></Link>
       <Routes>
         <Route path="/" element={<CreateButton />}/>
         <Route path="/create/:bucketUrl" element={<CreatedPage />}/>
         <Route path="/history/:bucketUrl" element={<History />}/>
       </Routes>
+      <footer>
+        <a href="https://www.vecteezy.com/free-vector/cartoon">Cartoon Vectors by Vecteezy</a>
+      </footer>
     </Router>
   )
 }
